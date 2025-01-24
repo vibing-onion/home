91a819dd5488807b3b4d87cc6f4419e6a7777500
Curtis <tcctsang@connect.ust.hk>
2025-01-24 14:38:05 +0800

    v1.4.2

--- asset/element/content.js
+++ asset/element/content.js
@@ -5,10 +5,14 @@ pageDescription_.innerHTML = pageDescription
 let focusBox = document.getElementById('focusBox')
 const focusArea = ['Web Scraping', 'Data Analytics', 'Database Management', 'User Experience', 'Software Developement', 'Machine Learning']
 
-focusArea.forEach((e) => {
-  let focus = document.createElement('div')
-  focus.innerHTML = e
-  focus.className = 'scrolling-text' + ' text' + (focusArea.indexOf(e)%3+1)
-  focus.style.lineHeight = 35 + 'px';
-  focusBox.appendChild(focus)
-})
+const loadFocus = () => {
+  focusArea.forEach((e) => {
+    let focus = document.createElement('div')
+    focus.innerHTML = e
+    focus.className = 'scrolling-text' + ' text' + (focusArea.indexOf(e)+1)
+    focus.style.lineHeight = 35 + 'px';
+    focusBox.appendChild(focus)
+  })
+}
+
+loadFocus()
